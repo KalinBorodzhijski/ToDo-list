4ba777f858e56961e2b055610d0b0e7eeb72f26f
kalin15342 <kalindobrinov@abv.bg>
2020-03-28 18:36:06 +0200

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-$("li").on("click",function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("crossed");
+});
+$("ul").on("click","span",function(event){
+   
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+$("#inputTask").on("keypress",function(event){
+    if(event.which == "13"){
+        var value = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class='delete'><i class='far fa-trash-alt'></i></span> " + value + "</li>");
+
+    }
+})
+$("i").on("click",function(){
+    
+    $("input").fadeToggle();
 })
